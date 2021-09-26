1aa3c786d31bf6c22e394b4c8fa3414bd6578401
AnanyaSairaj <91346938+AnanyaSairaj@users.noreply.github.com>
2021-09-25 22:48:56 -0600

    Update udp-client.c

--- CLIENT/udp-client.c
+++ CLIENT/udp-client.c
@@ -185,10 +185,10 @@ for (i=1; i<=total_frame;i++)
    
    fptr = fopen(filename, "rb");
    
-   if((f_size % BUF_SIZE) !=0)
-      total_frame = (f_size/BUF_SIZE)+1;//calculating the total frames by dividing the file size with the buffer and adding +1 if rem is not 0
+   if ((f_size % BUF_SIZE) !=0)
+	   total_frame = (f_size/BUF_SIZE)+1;//calculating the total frames by dividing the file size with the buffer and adding +1 if rem is not 0
    else 
-      total_frame= (f_size/BUF_SIZE);
+	   total_frame= (f_size/BUF_SIZE);
       
       printf("THe total frame size is %ld\n",total_frame);
       
